c5da212c544e91e646051882c0965a1a06a7dfac
fjplaurr <fjplaurr@gmail.com>
2019-05-10 23:59:43 +0200

    Added new features. Change buttons.

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -9,38 +9,42 @@ class QuoteMachine extends React.Component{
             color: ''
         }
         this.changeColor = this.changeColor.bind(this);
-        this.getRandomQUote = this.getRandomQUote.bind(this);
+        this.quote = this.getRandomQUote.bind(this);
     }
 
     componentDidMount(){
         this.getRandomQUote();
     }
 
+    componentDidUpdate(){ //Need this function to avoid async trouble due to state's updates
+        document.body.style.backgroundColor = this.state.color;
+    }
+
     render(){
         return(
             <React.Fragment>
-                    <h3 id="text">
-                        {this.state.quote}
-                    </h3>
-                    <h4 id="author">
-                        - {this.state.author}
-                    </h4>
+                <h3 id="text" style={{color: this.state.color}}>
+                    {this.state.quote}
+                </h3>
+                <h4 id="author" style={{color: this.state.color}}>
+                    - {this.state.author}
+                </h4>
                 <button type="button" className="btn btn-info" id="new-quote"
                     onClick = {()=>
-                        {this.getRandomQUote();
+                        {
+                            this.getRandomQUote();
                             this.changeColor();
-                        }}
+                        }
+                    }
                 >
                     Get a random quote
                 </button>
                 <a id="tweet-quote" target="_blank" rel="noopener noreferrer" href="http://twitter.com/intent/tweet">
                     <button type="button" className="btn btn-info">
-                   
-                    Tweet <i class="fab fa-twitter"></i>
+                        Tweet <i class="fab fa-twitter"></i>
                     </button>
                 </a>
-                <br/>
-                
+                <br/>             
             </React.Fragment>
         )
     }
@@ -53,14 +57,14 @@ class QuoteMachine extends React.Component{
                   let randomQuote = data.quotes[Math.floor(Math.random()*data.quotes.length)];
                   this.setState(
                       {
-                          quote: randomQuote.quote,
-                          author: randomQuote.author
+                        quote: randomQuote.quote,
+                        author: randomQuote.author
                       }
                   )
               }else{
                   return console.log('No quote was found')
               }
-        }).then(this.changeColor)
+        })
     }
 
     changeColor = () => {
@@ -70,15 +74,9 @@ class QuoteMachine extends React.Component{
 		  colory += letters[Math.floor(Math.random() * 16)];
 	    }
         this.setState(
-            {
-            color: colory
-            }
-        )
-        document.body.style.backgroundColor=this.state.color;
-
-
+            {color: colory} 
+        )   
     }
-
 
 }
  
